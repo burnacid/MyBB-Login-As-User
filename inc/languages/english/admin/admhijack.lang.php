c1b4fef4a74199cf225cf048df875b77afdb73ea
Stefan Lenders <stefan@lenders-it.nl>
2018-06-04 18:40:29 +0200

    Added settings and Language files

---
+++ inc/languages/english/admin/admhijack.lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$l['admhijack_name'] = 'Login As';
+$l['admhijack_desc'] = 'Allows admins to log into another user\'s account via their profile, and quickly swap back to their account via the Logout link. Original creator <a href="http://mybbhacks.zingaburga.com/showthread.php?tid=268">ZiNgA BuRgA</a></strong>';
+$l['setting_group_admhijack'] = "Login As User";
+$l['setting_group_admhijack_desc'] = 'Settings for logging in as user';
+
+$l['setting_loginasgroups'] = 'Allowed Groups';
+$l['setting_loginasgroups_desc'] = 'Which groups can use log in as a user';
+$l['setting_deniedforums'] = 'Denied Forums';
+$l['setting_deniedforums_desc'] = 'These forums cannot be accessed when logged in as another user.';
--- inc/plugins/admhijack.php
+++ inc/plugins/admhijack.php
@@ -10,12 +10,15 @@ $plugins->add_hook('private_start', 'admhijack_private_start');
 $plugins->add_hook('forumdisplay_start', 'admhijack_forumdisplay_start');
 $plugins->add_hook('showthread_start', 'admhijack_showthread_start');
 
+$lang->load('admhijack');
 
 function admhijack_info()
 {
+    global $lang;
+
     return array(
-        'name' => 'Login As',
-        'description' => 'Allows admins to log into another user\'s account via their profile, and quickly swap back to their account via the Logout link. Original creator <a href="http://mybbhacks.zingaburga.com/showthread.php?tid=268">ZiNgA BuRgA</a></strong>',
+        'name' => $lang->admhijack_name,
+        'description' => $lang->admhijack_desc,
         'website' => '',
         'author' => 'S. Lenders (burnacid)',
         'authorsite' => 'https://lenders-it.nl',
@@ -24,24 +27,131 @@ function admhijack_info()
         );
 }
 
+function admhijack_activate()
+{
+    global $db, $lang;
+
+    // Settings group array details
+    $group = array(
+        'name' => 'loginas',
+        'title' => $db->escape_string($lang->setting_group_admhijack),
+        'description' => $db->escape_string($lang->setting_group_admhijack_desc),
+        'isdefault' => 0);
+
+    // Check if the group already exists.
+    $query = $db->simple_select('settinggroups', 'gid', "name='loginas'");
+
+    if ($gid = (int)$db->fetch_field($query, 'gid')) {
+        // We already have a group. Update title and description.
+        $db->update_query('settinggroups', $group, "gid='{$gid}'");
+    } else {
+        // We don't have a group. Create one with proper disporder.
+        $query = $db->simple_select('settinggroups', 'MAX(disporder) AS disporder');
+        $disporder = (int)$db->fetch_field($query, 'disporder');
+
+        $group['disporder'] = ++$disporder;
+
+        $gid = (int)$db->insert_query('settinggroups', $group);
+    }
+
+    // Deprecate all the old entries.
+    $db->update_query('settings', array('description' => 'ADHIJACKDELETEMARKER'),
+        "gid='{$gid}'");
+
+    // add settings
+    $settings = array('deniedforums' => array('optionscode' => 'forumselect',
+                'value' => ''), 'loginasgroups' => array('optionscode' => 'groupselect', 'value' =>
+                '4'));
+
+    $disporder = 0;
+
+    // Create and/or update settings.
+    foreach ($settings as $key => $setting) {
+        // Prefix all keys with group name.
+        $key = "admhijack_{$key}";
+
+        $lang_var_title = "setting_{$key}";
+        $lang_var_description = "setting_{$key}_desc";
+
+        $setting['title'] = $lang->{$lang_var_title};
+        $setting['description'] = $lang->{$lang_var_description};
+
+        // Filter valid entries.
+        $setting = array_intersect_key($setting, array(
+            'title' => 0,
+            'description' => 0,
+            'optionscode' => 0,
+            'value' => 0,
+            ));
+
+        // Escape input values.
+        $setting = array_map(array($db, 'escape_string'), $setting);
+
+        // Add missing default values.
+        ++$disporder;
+
+        $setting = array_merge(array(
+            'description' => '',
+            'optionscode' => 'yesno',
+            'value' => 0,
+            'disporder' => $disporder), $setting);
+
+        $setting['name'] = $db->escape_string($key);
+        $setting['gid'] = $gid;
+
+        // Check if the setting already exists.
+        $query = $db->simple_select('settings', 'sid', "gid='{$gid}' AND name='{$setting['name']}'");
+
+        if ($sid = $db->fetch_field($query, 'sid')) {
+            // It exists, update it, but keep value intact.
+            unset($setting['value']);
+            $db->update_query('settings', $setting, "sid='{$sid}'");
+        } else {
+            // It doesn't exist, create it.
+            $db->insert_query('settings', $setting);
+            // Maybe use $db->insert_query_multiple somehow
+        }
+    }
+
+    // Delete deprecated entries.
+    $db->delete_query('settings', "gid='{$gid}' AND description='ADHIJACKDELETEMARKER'");
+
+    // This is required so it updates the settings.php file as well and not only the database - they must be synchronized!
+    rebuild_settings();
+}
+
+function admhijack_deactivate()
+{
+    global $db, $lang;
+
+    // Delete settings group
+    $db->delete_query('settinggroups', "name='loginas'");
+
+    // Remove the settings
+    $db->delete_query('settings', "name LIKE 'admhijack_*%'");
+
+    // This is required so it updates the settings.php file as well and not only the database - they must be synchronized!
+    rebuild_settings();
+}
+
 function admhijack_login()
 {
-    global $mybb;
+    global $mybb, $lang;
     if (!admhijack_allowed() || $mybb->input['do'] != 'hijack' || !$mybb->input['uid'])
         return;
 
     verify_post_check($mybb->input['my_post_key']);
     $user = get_user(intval($mybb->input['uid']));
     if (!$user)
-        error('Invalid UserID supplied.');
+        error($lang->admhijack_invalid_uid);
     my_setcookie('mybbadminuser', $mybb->user['uid'] . '_' . $mybb->user['loginkey'], null, true);
     my_setcookie('mybbuser', $user['uid'] . '_' . $user['loginkey'], null, true);
 
-    admhijack_log_admin_action("Start controlling:" . $user['username']);
+    admhijack_log_admin_action($lang->admhijack_start_controlling . ": " . $user['username']);
 
-    redirect('index.php', 'You have successfully logged in as ' .
-        htmlspecialchars_uni($user['username']) .
-        '<br />You will be redirected to the forum index...');
+    redirect('index.php', $lang->admhijack_login_success . ' ' .
+        htmlspecialchars_uni($user['username']) . '<br /> ' . $lang->
+        admhijack_redirect_index);
     exit;
 }
 
@@ -90,10 +200,10 @@ function admhijack_logout()
         verify_post_check($mybb->input['my_post_key']);
         $user = get_user(intval($mybb->input['uid']));
         if (!$user)
-            error('Invalid UserID supplied.');
+            error($lang->admhijack_invalid_uid);
         update_loginkey($user['uid']);
-        redirect('member.php?action=profile&uid=' . $user['uid'],
-            'You have successfully forced the selected user to log out.');
+        redirect('member.php?action=profile&uid=' . $user['uid'], $lang->
+            admhijack_force_logoff_success);
         exit;
     }
 
@@ -116,18 +226,19 @@ function admhijack_logout()
             error($lang->error_notloggedout);
     my_setcookie('mybbuser', $cookies['mybbadminuser'], null, true);
 
-    admhijack_log_admin_action("Stopped controlling:" . $mybb->user['username']);
+    admhijack_log_admin_action($lang->admhijack_stopped_controlling . ":" . $mybb->
+        user['username']);
 
     my_unsetcookie('mybbadminuser');
 
-    redirect('member.php?action=profile&uid=' . $mybb->user['uid'],
-        'You have logged out of the hijacked account and will be logged back in to your admin account.');
+    redirect('member.php?action=profile&uid=' . $mybb->user['uid'], $lang->
+        admhijack_logoff_success);
     exit;
 }
 
 function admhijack_profile()
 {
-    global $templates, $mybb;
+    global $templates, $mybb, $lang;
 
     if (!admhijack_allowed()) {
         return;
@@ -143,8 +254,8 @@ function admhijack_profile()
 <tr>
 <td class="trow1">
 <ul>
-<li><a href="{$mybb->settings[\'bburl\']}/member.php?action=login&amp;do=hijack&amp;uid={$uid}&amp;my_post_key={$mybb->post_code}">Log in as this user</a></li>
-<li><a href="{$mybb->settings[\'bburl\']}/member.php?action=logout&amp;do=regenkey&amp;uid={$uid}&amp;my_post_key={$mybb->post_code}">Force this user to log out (if logged in)</a></li>
+<li><a href="{$mybb->settings[\'bburl\']}/member.php?action=login&amp;do=hijack&amp;uid={$uid}&amp;my_post_key={$mybb->post_code}">{$lang->admhijack_login_as}</a></li>
+<li><a href="{$mybb->settings[\'bburl\']}/member.php?action=logout&amp;do=regenkey&amp;uid={$uid}&amp;my_post_key={$mybb->post_code}">{$lang->admhijack_force_logoff}</a></li>
 </ul>
 </td>
 </tr>
@@ -156,34 +267,38 @@ function admhijack_allowed()
 {
     global $mybb;
 
-    $allowed = $mybb->settings['loginasgroups'];
+    $allowed = explode(",", $mybb->settings['admhijack_loginasgroups']);
     $othergroups = explode(",", $mybb->user['additionalgroups']);
 
-    if ($mybb->user['usergroup'] == $allowed) {
-        return true;
-    } elseif (in_array($allowed, $othergroups)) {
+    if (in_array($mybb->user['usergroup'], $allowed)) {
         return true;
     } else {
+        foreach ($othergroups as $group) {
+            if (in_array($group, $allowed)) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
 
 function admhijack_private_start()
 {
-    global $mybb;
+    global $mybb,$lang;
 
     $cookies = &$_COOKIE;
 
     if (!empty($cookies['mybbadminuser'])) {
         redirect($mybb->settings['bburl'],
-            "GO AWAY... You are not allowed to look at someone else his private messages...",
-            "Permissions Denied", false);
+            $lang->admhijack_permission_denied_pm,
+            $lang->admhijack_permission_denied, false);
     }
 }
 
 function admhijack_forumdisplay_start()
 {
-    global $mybb;
+    global $mybb,$lang;
 
     $cookies = &$_COOKIE;
 
@@ -192,15 +307,15 @@ function admhijack_forumdisplay_start()
 
         if (in_array($mybb->input['fid'], $deniedfid)) {
             redirect($mybb->settings['bburl'],
-                "GO AWAY... You are not allowed to look at these forums through someone else his account...",
-                "Permissions Denied", false);
+                $lang->admhijack_permission_denied_forums,
+                $lang->admhijack_permission_denied, false);
         }
     }
 }
 
 function admhijack_showthread_start()
 {
-    global $mybb, $fid;
+    global $mybb, $fid,$lang;
 
     $cookies = &$_COOKIE;
 
@@ -209,8 +324,8 @@ function admhijack_showthread_start()
 
         if (in_array($fid, $deniedfid)) {
             redirect($mybb->settings['bburl'],
-                "GO AWAY... You are not allowed to look at these forums through someone else his account...",
-                "Permissions Denied", false);
+                $lang->admhijack_permission_denied_forums,
+                $lang->admhijack_permission_denied, false);
         }
     }
 }
