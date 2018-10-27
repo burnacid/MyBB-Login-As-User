69d73a820bac2e9b4feead5424c7e3e607060e19
Stefan Lenders <stefan@lenders-it.nl>
2018-10-27 10:40:18 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MyBB Login As User/Frontend Language
    Translate-URL: http://weblate.lenders-it.nl/projects/mybb-login-user/frontend-language/dutch/

--- inc/languages/dutch/admhijack.lang.php
+++ inc/languages/dutch/admhijack.lang.php
@@ -1,13 +1,13 @@
 <?php
-$l['admhijack_invalid_uid'] = 'Invalid UserID supplied.';
-$l['admhijack_force_logoff_success'] = 'You have successfully forced the selected user to log out.';
-$l['admhijack_stopped_controlling'] = 'Stopped controlling';
-$l['admhijack_start_controlling'] = 'Start controlling';
-$l['admhijack_logoff_success'] = 'You have logged out of the hijacked account and will be logged back in to your admin account.';
-$l['admhijack_login_as'] = 'Log in as this user';
-$l['admhijack_force_logoff'] = 'Force this user to log out (if logged in)';
-$l['admhijack_login_success'] = 'You have successfully logged in as';
-$l['admhijack_redirect_index'] = 'You will be redirected to the forum index...';
-$l['admhijack_permission_denied'] = 'Permissions Denied';
-$l['admhijack_permission_denied_forums'] = 'GO AWAY... You are not allowed to look at these forums through someone else his account...';
-$l['admhijack_permission_denied_pm'] = "GO AWAY... You are not allowed to look at someone else his private messages...";
+$l['admhijack_invalid_uid'] = 'Incorrect gebruikers ID opgegeven.';
+$l['admhijack_force_logoff_success'] = 'De gebruiker is succesvol geforceerd uitgelogd.';
+$l['admhijack_stopped_controlling'] = 'Gestopt met controleren';
+$l['admhijack_start_controlling'] = 'Controleren gestart';
+$l['admhijack_logoff_success'] = 'U bent uitgelogd van het overgenomen account en bent terug ingelogd met uw administrator account.';
+$l['admhijack_login_as'] = 'Inloggen als deze gebruiker';
+$l['admhijack_force_logoff'] = 'Forceer deze gebruiker om uit te loggen (wanneer hij is ingelogd)';
+$l['admhijack_login_success'] = 'U bent succesvol ingelogd als';
+$l['admhijack_redirect_index'] = 'U wordt terug gestuurd naar de forum beginpagina...';
+$l['admhijack_permission_denied'] = 'Rechten geweigerd';
+$l['admhijack_permission_denied_forums'] = 'GA WEG... Je mag deze forums niet bezoeken via iemand anders zijn of haar account...';
+$l['admhijack_permission_denied_pm'] = "GA WEG... Je mag niet iemand zijn of haar prive berichten bekijken...";
