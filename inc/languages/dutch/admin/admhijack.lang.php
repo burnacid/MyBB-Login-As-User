8c6b04a352a410a6bd97ca535307ff99223dca6f
Stefan Lenders <stefan@lenders-it.nl>
2018-10-27 11:41:48 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: MyBB Login As User/Admin CP Language
    Translate-URL: http://weblate.lenders-it.nl/projects/mybb-login-user/admin-cp-language/dutch/

--- inc/languages/dutch/admin/admhijack.lang.php
+++ inc/languages/dutch/admin/admhijack.lang.php
@@ -1,9 +1,9 @@
 <?php
-$l['admhijack_name'] = 'Login As';
-$l['admhijack_desc'] = 'Allows admins to log into another user\'s account via their profile, and quickly swap back to their account via the Logout link. <strong>Original creator <a href="http://mybbhacks.zingaburga.com/showthread.php?tid=268">ZiNgA BuRgA</a></strong>';
-$l['setting_group_admhijack'] = "Login As User";
-$l['setting_group_admhijack_desc'] = 'Settings for logging in as user';
-$l['setting_admhijack_loginasgroups'] = 'Allowed Groups';
-$l['setting_admhijack_loginasgroups_desc'] = 'Which groups can use log in as a user';
-$l['setting_admhijack_deniedforums'] = 'Denied Forums';
-$l['setting_admhijack_deniedforums_desc'] = 'These forums cannot be accessed when logged in as another user.';
+$l['admhijack_name'] = 'Login Als';
+$l['admhijack_desc'] = 'Geeft administrators de mogelijkheid om via een gebruiker\'s profile in te loggen als de gebruiker<strong>Origineel gemaakt door <a href="http://mybbhacks.zingaburga.com/showthread.php?tid=268">ZiNgA BuRgA</a></strong>';
+$l['setting_group_admhijack'] = "Inloggen als Gebruiker";
+$l['setting_group_admhijack_desc'] = 'Instellingen voor Inloggen als Gebruiker';
+$l['setting_admhijack_loginasgroups'] = 'Groepen Toegestaan';
+$l['setting_admhijack_loginasgroups_desc'] = 'Welke groepen kunnen inloggen als iemand anders';
+$l['setting_admhijack_deniedforums'] = 'Verboden Forums';
+$l['setting_admhijack_deniedforums_desc'] = 'Deze forums kunnen niet bekeken worden wanneer iemand is ingelogd als een andere gebruiker.';
