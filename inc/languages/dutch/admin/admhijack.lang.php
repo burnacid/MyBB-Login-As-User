bc7dc2c18aea35157ad169c22f92877d82be317f
Stefan Lenders <stefan@lenders-it.nl>
2018-10-27 11:40:52 +0000

    Added translation using Weblate (Dutch)

---
+++ inc/languages/dutch/admin/admhijack.lang.php
@@ -0,0 +1,9 @@
+<?php
+$l['admhijack_name'] = 'Login As';
+$l['admhijack_desc'] = 'Allows admins to log into another user\'s account via their profile, and quickly swap back to their account via the Logout link. <strong>Original creator <a href="http://mybbhacks.zingaburga.com/showthread.php?tid=268">ZiNgA BuRgA</a></strong>';
+$l['setting_group_admhijack'] = "Login As User";
+$l['setting_group_admhijack_desc'] = 'Settings for logging in as user';
+$l['setting_admhijack_loginasgroups'] = 'Allowed Groups';
+$l['setting_admhijack_loginasgroups_desc'] = 'Which groups can use log in as a user';
+$l['setting_admhijack_deniedforums'] = 'Denied Forums';
+$l['setting_admhijack_deniedforums_desc'] = 'These forums cannot be accessed when logged in as another user.';
