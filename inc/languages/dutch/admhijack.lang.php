0da59c91805a1efc4d326107043e311af5d16bc1
Stefan Lenders <stefan@lenders-it.nl>
2018-10-27 10:39:05 +0000

    Added translation using Weblate (Dutch)

---
+++ inc/languages/dutch/admhijack.lang.php
@@ -0,0 +1,13 @@
+<?php
+$l['admhijack_invalid_uid'] = 'Invalid UserID supplied.';
+$l['admhijack_force_logoff_success'] = 'You have successfully forced the selected user to log out.';
+$l['admhijack_stopped_controlling'] = 'Stopped controlling';
+$l['admhijack_start_controlling'] = 'Start controlling';
+$l['admhijack_logoff_success'] = 'You have logged out of the hijacked account and will be logged back in to your admin account.';
+$l['admhijack_login_as'] = 'Log in as this user';
+$l['admhijack_force_logoff'] = 'Force this user to log out (if logged in)';
+$l['admhijack_login_success'] = 'You have successfully logged in as';
+$l['admhijack_redirect_index'] = 'You will be redirected to the forum index...';
+$l['admhijack_permission_denied'] = 'Permissions Denied';
+$l['admhijack_permission_denied_forums'] = 'GO AWAY... You are not allowed to look at these forums through someone else his account...';
+$l['admhijack_permission_denied_pm'] = "GO AWAY... You are not allowed to look at someone else his private messages...";
